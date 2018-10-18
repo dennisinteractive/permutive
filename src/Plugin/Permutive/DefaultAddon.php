4a5414866ceba6c8e7aa8646a73f2367f26dbe2d
Peter Clarke <peter_clarke@dennis.co.uk>
2018-10-18 17:39:05 +0100

    The default addon

--- src/Plugin/Permutive/DefaultAddon.php
+++ src/Plugin/Permutive/DefaultAddon.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\permutive\Plugin\Permutive;
 
+use Drupal\node\NodeInterface;
 use Drupal\permutive\Plugin\PermutiveBase;
 
 /**
@@ -20,7 +21,33 @@ class DefaultAddon extends PermutiveBase {
    * {@inheritdoc}
    */
   public function getData() {
-    $data['page']['publisher']['name'] = 'Foooooo';
+    $route_match = \Drupal::routeMatch();
+    $token = \Drupal::token();
+    $name = $token->replace(
+      '[site:name]',
+      [],
+      ['clear' => TRUE]
+    );
+    $data['page']['publisher']['name'] = $name;
+
+    // Node values.
+    $node = $route_match->getParameter('node');
+    if ($node instanceof NodeInterface) {
+      $data['content'] = [
+        'headline' => $token->replace(
+          '[node:title]',
+          ['node' => $node],
+          ['clear' => TRUE]
+        ),
+        'description' => $token->replace(
+          '[node:summary]',
+          ['node' => $node],
+          ['clear' => TRUE]
+        ),
+        'type' => $node->getEntityTypeId(),
+      ];
+    }
+
     return $data;
   }
 
